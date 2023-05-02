77616c4a979097655e0f6d75342a540a2443e343
Andre Solci Muradas <adrsolci@gmail.com>
2023-05-01 23:19:57 -0300

    JS slide btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 let btnLeft = document.querySelector('#slideLeft');
 let btnRigth = document.querySelector('#slideRight');
 let slideContainer = document.querySelector('.slider_container');
+let slideSize = document.querySelector('.slide').clientWidth;
+let totalSlide = slideContainer.clientWidth / slideSize;
+let slidePosition = 0;
 
 btnLeft.addEventListener('click', slidePrevious);
-
 btnRigth.addEventListener('click', slideFoward);
+
+function checkCount()
+{
+    if(slidePosition == totalSlide)
+    {
+        slidePosition = 0;
+    }
+    else if(slidePosition < 0)
+    {
+        slidePosition = totalSlide - 1;
+    }
+    console.log(slidePosition);
+}
 
 function slidePrevious()
 {
-    slideContainer.style.marginLeft = '-1920px';
+    slidePosition -= 1; 
+    checkCount();
+    slideContainer.style.marginLeft = (slidePosition * slideSize)+'px';
 }
 
 function slideFoward()
 {
-    console.log("clicou direito");
+    slidePosition += 1; 
+    slideContainer.style.marginLeft = -(slidePosition * slideSize)+'px';
+    checkCount();
 }
